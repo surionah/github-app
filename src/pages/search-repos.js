1f1c48d2ce2dc3dd5facd50b51692535621581b3
Sergio Uriona Herbas <seruriher@gmail.com>
2021-02-05 12:12:32 -0400

    Add spinner when search is running

--- src/pages/search-repos.js
+++ src/pages/search-repos.js
@@ -3,6 +3,7 @@ import {
   useEffect,
   useContext
 } from 'react';
+import { Spinner } from 'react-bootstrap';
 import axios from 'axios';
 
 import PageTitle from '../components/title';
@@ -16,14 +17,17 @@ function SearchRepos() {
   const appContext = useContext(AppContext);
   const [reposList, setReposList] = useState(null);
   const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false)
 
   async function loadRepos() {
     if (appContext.criteria !== '') {
+      setIsLoading(true);
       try {
         const usersResult = await axios.get
           (`${process.env.REACT_APP_BASE_URL}repositories?q=${appContext.criteria} in:name&per_page=10&page=${page}`);
         const nonRepeatedList = new Set([...reposList, ...usersResult.data.items]);
         setReposList(Array.from(nonRepeatedList));
+        setIsLoading(false);
       } catch(e) {
         console.error(e);
       }
@@ -50,6 +54,13 @@ function SearchRepos() {
       <PageTitle content="Repositories" />
       <PageDescription content="In this page you can search for created repositories on Github." />
       <Search search={() => setReposList([])} />
+      {
+        isLoading && (
+          <div className="d-flex justify-content-center">
+            <Spinner animation="border" role="status" />
+          </div>
+        )
+      }
       <InfiniteScrollList
         list={reposList}
         itemNameAttribute="name"
--- src/pages/search-users.js
+++ src/pages/search-users.js
@@ -3,6 +3,7 @@ import {
   useEffect,
   useContext
 } from 'react';
+import { Spinner } from 'react-bootstrap';
 import axios from 'axios';
 
 import PageTitle from '../components/title';
@@ -16,14 +17,17 @@ function SearchUsers() {
   const appContext = useContext(AppContext);
   const [usersList, setUsersList] = useState(null);
   const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false)
 
   async function loadUsers() {
     if (appContext.criteria !== '') {
+      setIsLoading(true);
       try {
         const usersResult = await axios.get
           (`${process.env.REACT_APP_BASE_URL}users?q=${appContext.criteria} in:login&per_page=10&page=${page}`);
         const nonRepeatedList = new Set([...usersList, ...usersResult.data.items]);
         setUsersList(Array.from(nonRepeatedList));
+        setIsLoading(false);
       } catch(e) {
         console.error(e);
       }
@@ -50,6 +54,13 @@ function SearchUsers() {
       <PageTitle content="Users" />
       <PageDescription content="In this page you can saerch for registered users on Github." />
       <Search search={() => setUsersList([])} />
+      {
+        isLoading && (
+          <div className="d-flex justify-content-center">
+            <Spinner animation="border" role="status" />
+          </div>
+        )
+      }
       <InfiniteScrollList
         list={usersList}
         itemNameAttribute="login"
